601dcd40534421b74c25a70f823f95ad11a7904c
MRVAPORWAVE25 <101008696+MRVAPORWAVE25@users.noreply.github.com>
2026-05-18 20:28:21 -0400

    Implement file merging and fetching for Bombun

--- Bombun.js
+++ Bombun.js
@@ -874,7 +874,51 @@ const Engine = (function () {
 				return Promise.resolve();
 			},
 		};
+			function mergeFiles(fileParts) {
+    return new Promise((resolve, reject) => {
+        let buffers = [];
 
+        function fetchPart(index) {
+            if (index >= fileParts.length) {
+                let mergedBlob = new Blob(buffers);
+                let mergedFileUrl = URL.createObjectURL(mergedBlob);
+                resolve(mergedFileUrl);
+                return;
+            }
+            fetch(fileParts[index]).then((response) => {
+                if (!response.ok) throw new Error("Missing part: " + fileParts[index]);
+                return response.arrayBuffer();
+            }).then((data) => {
+                buffers.push(data);
+                fetchPart(index + 1);
+            }).catch(reject);
+        }
+        fetchPart(0);
+    });
+}
+
+function getParts(file, start, end) {
+    let parts = [];
+    for (let i = start; i <= end; i++) {
+        parts.push(file + ".part" + i);
+    }
+    return parts;
+}
+Promise.all([
+    mergeFiles(getParts("Bombun.pck", 1, 35)),
+    mergeFiles(getParts("Bombun.wasm", 1, 2))
+]).then(([pckUrl, wasmUrl]) => {
+    window.fetch = async function (url, ...args) {
+        if (url.endsWith("Bombun.pck")) {
+            return originalFetch(pckUrl, ...args);
+        } else if (url.endsWith("Bombun.wasm")) {
+            return originalFetch(wasmUrl, ...args);
+        } else {
+            return originalFetch(url, ...args);
+        }
+    };
+    window.godotRunStart();
+});
 		Engine.prototype = proto;
 		// Closure compiler exported instance methods.
 		Engine.prototype['init'] = Engine.prototype.init;
